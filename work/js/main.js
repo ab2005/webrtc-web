6aa52a504e737640327755f42a76c675e5742589
AB <alexandre.barilov@nauto.com>
2016-09-25 18:03:05 -0700

    Connect stream

--- work/js/main.js
+++ work/js/main.js
@@ -1,2 +1,24 @@
 'use strict';
 
+var video = document.querySelector('video');
+
+navigator.getUserMedia = navigator.getUserMedia ||
+    navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+var constraints = {
+  audio: false,
+  video: true
+};
+function onMediaStreamCreated(stream) {
+  window.stream = stream; // stream available to console
+  if (window.URL) {
+    video.src = window.URL.createObjectURL(stream);
+  } else {
+    video.src = stream;
+  }
+}
+function errorCallback(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
+
+navigator.getUserMedia(constraints, onMediaStreamCreated, errorCallback);
